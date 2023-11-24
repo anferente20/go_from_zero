0864756125025215dc32ca62fe96004cb91ab034
Renvel97 <150822406+Renvel97@users.noreply.github.com>
2023-11-24 10:04:45 -0500

    add main
    * first import
    * add variables file
    * show integers

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go_from_zero/variables"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	variables.ShowIntegers()
+}
---
+++ variables/variables.go
@@ -0,0 +1,14 @@
+package variables
+
+import "fmt"
+
+func ShowIntegers() {
+	var commonInteger int
+	integer32 := int32(10)
+	integer64 := int64(100)
+	fmt.Println("--------------------------------")
+	fmt.Println("----------- Integers -----------")
+	fmt.Println("Common Integer = ", commonInteger)
+	fmt.Println("32-bit Integer = ", integer32)
+	fmt.Println("645-bit Integer = ", integer64)
+}
